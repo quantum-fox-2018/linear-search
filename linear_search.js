c2336e12f2a7204e04c280c3c6bbb16030ba1507
WikaSilo <wikanyaa@gmail.com>
2018-03-06 21:29:36 +0700

    WikaSilo

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,17 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let countTarget = 0
+  for(let i=0; i<values.length; i++) {
+    if(target == values[i]) {
+      countTarget += 1
+    }
+  }
+  if(countTarget == 0) {
+    return -1
+  } else {
+    return countTarget
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +28,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let indexTargets = []
+  for(let i=0; i<values.length; i++) {
+    if(target == values[i]) {
+      indexTargets.push(i)
+    }
+  }
+  return indexTargets
 }
 
 let banana_arr = "banana".split("");
