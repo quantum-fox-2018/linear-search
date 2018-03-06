588ed0bf9819ac29b2dd12f49291c4ae819ce8f2
agungcaproex <agung.caproex@gmail.com>
2018-03-06 13:54:11 +0700

    fourth commit day 2

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,13 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(let i=0; i<values.length; i++){
+    // console.log(values[i])
+    if(values[i] == target){
+      return i
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +24,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let arrIndex = []
+  for(let i=0; i<values.length; i++){
+    if(values[i] == target){
+      arrIndex.push(i)
+    }
+  }
+  return arrIndex
 }
 
 let banana_arr = "banana".split("");
