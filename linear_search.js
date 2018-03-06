5ce70b0ebb7121b719c25d5bb345a1f31cc15ef4
JoviAndrew <jovian_andrew@hotmail.com>
2018-03-06 13:53:57 +0700

    submit linear search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,10 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(let counter = 0; counter<values.length; counter++){
+    if(target === values[counter]) return counter;
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +21,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let result = [];
+  
+  for(let counter = 0; counter<values.length; counter++){
+    if(target === values[counter]) result.push(counter);
+  }
+  if(result.length === 0) return -1
+  return result;
 }
 
 let banana_arr = "banana".split("");
