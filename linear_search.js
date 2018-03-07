0ad8e3b62b6982ecb9f9dbd22dee16d811b88094
NeilTanado <neiltanado@yahoo.co.id>
2018-03-07 11:51:38 +0700

    fix error

--- linear_search.js
+++ linear_search.js
@@ -3,23 +3,17 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
-  var values = 0;
-  for(var i = 0 ; i < random_numbers.length ; i++){
-    if(random_numbers[i] === target){
-      values++;
+    for(var i = 0 ; i < random_numbers.length ; i++){
+      if(random_numbers[i] === target){
+        return i;
+      }
     }
-  }
-  if(values === 0){
     return -1;
-  }else{
-    return values;
-  }
-}
+  };
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, random_numbers));
+console.log(linearSearch(72, random_numbers));
 // 2
 console.log(linearSearch(9, random_numbers));
 // -1
